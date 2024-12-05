d751b0035bc8e1f0debd61764da413e743cf997a
Kent <kent.yuan@Gmail.com>
2024-12-05 08:32:40 +0100

    Day 05 ✅

--- src/Day05.kt
+++ src/Day05.kt
@@ -5,17 +5,46 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
-        return 0
+    fun parseInput(input: List<String>): Pair<List<Pair<Int, Int>>, List<List<Int>>> {
+        val (ruleLines, updateLines) = input.partition { '|' in it }
+        val rules = ruleLines.map { it.split("|").let { it[0].toInt() to it[1].toInt() } }
+        val updates = updateLines.drop(1).map { it.toInts(sep = ",") }
+        return rules to updates
     }
 
-    fun part2(input: List<String>): Long {
-        return 0
+    fun correctUpdates(updates: List<List<Int>>, rules: List<Pair<Int, Int>>) = updates.filter { update ->
+        var myRules = rules.filter { rule -> rule.first in update && rule.second in update }
+        update.asSequence().map { n ->
+            myRules = myRules.filter { it.first != n }
+            myRules.none { it.second == n }
+        }.all { it }
     }
 
-    chkTestInput(part1(testInput), 0L, Part1)
+    fun part1(input: List<String>): Int {
+        val (rules, updates) = parseInput(input)
+        return correctUpdates(updates, rules).sumOf { it[it.size / 2] }
+    }
+
+    fun part2(input: List<String>): Int {
+        val (rules, updates) = parseInput(input)
+        val tobeFixed = (updates - correctUpdates(updates, rules).toSet())
+        return tobeFixed.map { it.toMutableList() }.sumOf { update ->
+            for (i in update.indices) {
+                for (j in i + 1..update.lastIndex) {
+                    val (current, later) = update[i] to update[j]
+                    if (rules.any { rule -> rule.first == later && rule.second == current }) {//swap
+                        update[i] = later
+                        update[j] = current
+                    }
+                }
+            }
+            update[update.size / 2]
+        }
+    }
+
+    chkTestInput(part1(testInput), 143, Part1)
     println("[Part1]: ${part1(input)}")
 
-    chkTestInput(part2(testInput), 0L, Part2)
+    chkTestInput(part2(testInput), 123, Part2)
     println("[Part2]: ${part2(input)}")
 }
