bd9393ceb354f86d840764e8ac5303cbe3f51feb
Kent <kent.yuan@Gmail.com>
2024-12-08 09:28:02 +0100

    Day 08 ✅

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,3 +1,6 @@
+import kotlin.math.abs
+import kotlin.math.max
+
 // https://adventofcode.com/2024/day/8
 fun main() {
     val today = "Day08"
@@ -5,17 +8,32 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
-        return 0
-    }
+    fun toMatrix(input: List<String>): MatrixDay08 = buildMap {
+        input.forEachIndexed { y, line -> line.forEachIndexed { x, c -> put(x to y, c) } }
+    }.let { points -> MatrixDay08(input[0].lastIndex, input.lastIndex, NotNullMap(points)) }
 
-    fun part2(input: List<String>): Long {
-        return 0
-    }
+    fun part1(input: List<String>): Int = toMatrix(input).countAntiNodes(Part1)
 
-    chkTestInput(Part1, testInput, 0L) { part1(it) }
+    fun part2(input: List<String>): Int = toMatrix(input).countAntiNodes(Part2)
+
+    chkTestInput(Part1, testInput, 14) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0L) { part2(it) }
+    chkTestInput(Part2, testInput, 34) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+class MatrixDay08(maxX: Int, maxY: Int, override val points: NotNullMap<Pair<Int, Int>, Char>) : Matrix<Char>(maxX, maxY, points) {
+    fun countAntiNodes(part1or2: String) = buildSet {
+        points.entries.filter { it.value != '.' }.map { it.value to it.key }.groupBy({ it.first }, { it.second }).values
+            .forEach { pList ->
+                pList.forEach { posA -> //antiNodes for each pos (posA)
+                    (pList - setOf(posA)).forEach { posB ->
+                        val (x, y) = (posA.first - posB.first) to (posA.second - posB.second)
+                        val factors = if (part1or2 == Part1) 1..1 else 0..max(maxX / abs(x), maxY / abs(y))
+                        factors.forEach { factor -> add(posA.first + x * factor to posA.second + y * factor) }
+                    }
+                }
+            }
+    }.count { it.validPoint() }
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,7 +1,5 @@
 import Direction.*
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
 import java.util.*
 import kotlin.time.measureTime
 
@@ -18,7 +16,8 @@ fun readInputAsInts(name: String) = File("src/inputs", "$name.txt").readLines().
 
 fun String.toInts(sep: String = " ") = split(sep.toRegex()).map { it.toInt() }
 fun String.toLongs(sep: String = " ") = split(sep.toRegex()).map { it.toLong() }
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun <T : Any?> T.alsoLog() = this.also { println(it) }
 
 fun <T> chkTestInput(part: String, testInput: List<String>, expected: T, solveIt: (List<String>) -> T) = measureTime {
     val actual = solveIt(testInput)
@@ -31,6 +30,9 @@ fun <T> solve(part: String, input: List<String>, solveIt: (List<String>) -> T) =
     print("[RESULT::$part]: $result")
 }.also { time -> println("    (${time})\n") }
 
+// list
+fun List<Char>.joinChars() = joinToString(separator = "") { "$it" }
+
 fun <T> List<List<T>>.transpose(): List<List<T>> {
     val result = (first().indices).map { mutableListOf<T>() }.toMutableList()
     forEach { list -> result.zip(list).forEach { it.first.add(it.second) } }
@@ -42,6 +44,22 @@ fun <T> List<T>.prepend(e: T): List<T> = buildList {
     addAll(this@prepend)
 }
 
+fun <T> permutations(input: List<T>): List<List<T>> {
+    fun permutationsRecursive(input: List<T>, index: Int, answers: MutableList<List<T>>) {
+        if (index == input.lastIndex) answers.add(input.toList())
+        for (i in index..input.lastIndex) {
+            Collections.swap(input, index, i)
+            permutationsRecursive(input, index + 1, answers)
+            Collections.swap(input, i, index)
+        }
+    }
+
+    val solutions = mutableListOf<List<T>>()
+    permutationsRecursive(input, 0, solutions)
+    return solutions
+}
+
+//map
 class MutableNotNullMap<K, V>(private val map: MutableMap<K, V>) : MutableMap<K, V> by map {
     override operator fun get(key: K): V {
         return checkNotNull(map[key]) { "Key ($key) not found in the NeverNullMap" }
@@ -54,14 +72,14 @@ class NotNullMap<K, V>(private val map: Map<K, V>) : Map<K, V> by map {
     }
 }
 
-fun <T : Any?> T.alsoLog() = this.also { println(it) }
-fun List<Char>.joinChars() = joinToString(separator = "") { "$it" }
+//Matrix related:
 open class Matrix<T : Any>(val maxX: Int, val maxY: Int, open val points: Map<Pair<Int, Int>, T>) {
 
     protected infix fun singleWithValue(value: T) = points.entries.first { it.value == value }
     protected infix fun withValue(value: T) = points.filterValues { it == value }
 
     protected fun Pair<Int, Int>.validPoint() = first in 0..maxX && second in 0..maxY
+    operator fun contains(pos: Pair<Int, Int>) = pos.validPoint()
     protected fun Pair<Int, Int>.move(direction: Direction) = when (direction) {
         Up -> first to (second - 1)
         Down -> first to (second + 1)
@@ -83,19 +101,4 @@ enum class Direction {
 
     fun turn90() = ordinal.let { entries[if (it == 3) 0 else it + 1] }
     fun turn90Back() = ordinal.let { entries[if (it == 0) 3 else it - 1] }
-}
-
-fun <T> permutations(input: List<T>): List<List<T>> {
-    fun permutationsRecursive(input: List<T>, index: Int, answers: MutableList<List<T>>) {
-        if (index == input.lastIndex) answers.add(input.toList())
-        for (i in index..input.lastIndex) {
-            Collections.swap(input, index, i)
-            permutationsRecursive(input, index + 1, answers)
-            Collections.swap(input, i, index)
-        }
-    }
-
-    val solutions = mutableListOf<List<T>>()
-    permutationsRecursive(input, 0, solutions)
-    return solutions
 }
