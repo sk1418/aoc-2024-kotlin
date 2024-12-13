a2401b3c8c9fe7a24a1696d3885126c151a67635
Kent <kent.yuan@Gmail.com>
2024-12-13 19:52:17 +0100

    Day 13 ✅

--- src/Day13.kt
+++ src/Day13.kt
@@ -5,17 +5,30 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
-        return 0
-    }
+    val reXY = """X.(\d+).*?Y.(\d+)""".toRegex()
+    fun parse(input: List<String>) = input.filter { it.isNotBlank() }
+        .chunked(3).map { iii -> iii.map { reXY.find(it).let { it!!.groupValues.let { it[1].toInt() to it[2].toInt() } } } }
 
-    fun part2(input: List<String>): Long {
-        return 0
-    }
+    fun part1(input: List<String>) = parse(input).sumOf { play(it[0], it[1], it[2]) }
 
-    chkTestInput(Part1, testInput, 0L) { part1(it) }
+    fun part2(input: List<String>) = parse(input).sumOf { play(it[0], it[1], it[2], 10000000000000L) }
+
+    chkTestInput(Part1, testInput, 480L) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0L) { part2(it) }
+//Not required:    chkTestInput(Part2, testInput, 0L) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+fun play(a: Pair<Int, Int>, b: Pair<Int, Int>, target: Pair<Int, Int>, addition: Long = 0): Long {
+    val (ax, ay) = a
+    val (bx, by) = b
+    val prizeX = target.first + addition
+    val prizeY = target.second + addition
+    // ca * ax + cb * bx = prizeX
+    // ca * ay + cb * by = prizeY
+    val cb = (ax * prizeY - ay * prizeX) / (ax * by - ay * bx)
+    val ca = (prizeY - by * cb) / ay
+    val ok = ca * ax + bx * cb == prizeX
+    return if (ok) ca * 3 + cb else 0
 }
