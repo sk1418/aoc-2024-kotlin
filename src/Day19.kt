7d700a1d3be9201b6f5a15ec9ce29a6e5367b6db
Kent <kent.yuan@Gmail.com>
2024-12-19 14:45:19 +0100

    Day 19 ✅

--- src/Day19.kt
+++ src/Day19.kt
@@ -5,17 +5,31 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
-        return 0
+    fun parseInput(input: List<String>): Pair<List<String>, List<String>> = input[0].split(", ").toList() to input.drop(2)
+
+    fun part1(input: List<String>): Int {
+        val (patterns, designs) = parseInput(input)
+        val re = patterns.joinToString(prefix = "^(", postfix = ")+$", separator = "|").toRegex()
+        return designs.count {re in it}
+    }
+
+    val cache: MutableMap<String, Long> = mutableMapOf()
+    fun allPossibleCount(patterns: List<String>, design: String): Long {
+        return cache.getOrPut(design) {
+            if (design.isEmpty()) 1 else
+                patterns.filter { design.startsWith(it) }.sumOf { p -> allPossibleCount(patterns, design.drop(p.length)) }
+        }
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val (patterns, designs) = parseInput(input)
+        return designs.sumOf { cache.clear(); allPossibleCount(patterns, it) }
     }
 
-    chkTestInput(Part1, testInput, 0L) { part1(it) }
+
+    chkTestInput(Part1, testInput, 6) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0L) { part2(it) }
+    chkTestInput(Part2, testInput, 16L) { part2(it) }
     solve(Part2, input) { part2(it) }
 }
